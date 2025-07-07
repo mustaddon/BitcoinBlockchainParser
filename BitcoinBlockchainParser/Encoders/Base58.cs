0eb76973dc67819ab08546e71501c7beaa930756
Leonid Salavatov <mustaddon@gmail.com>
2025-07-07 20:13:10 +0300

    Base58Address

--- BitcoinBlockchainParser/Encoders/Base58.cs
+++ BitcoinBlockchainParser/Encoders/Base58.cs
@@ -5,8 +5,17 @@ namespace BitcoinBlockchainParser.Encoders;
 
 public static class Base58
 {
+    private static readonly Base58Encoder _encoder = new();
+
     public static string ToBase58(this byte[] bytes) => _encoder.EncodeData(bytes);
 
-    private static readonly Base58Encoder _encoder = new();
+    public static string? ToBase58Address(this byte[] bytes, byte type = 0)
+    {
+        if (bytes.Length != 20)
+            return null;
+
+        var checksum = Hashes.HASH256(bytes = [type, .. bytes])[0..4];
+        return ToBase58([.. bytes, .. checksum]);
+    }
 }
 
